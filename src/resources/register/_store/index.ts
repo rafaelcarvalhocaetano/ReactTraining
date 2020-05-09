b5bfab89513dd4cea8257b60636842ae6d2e4a5b
rafael <rapha.pse@outlook.com>
2020-05-09 20:49:08 -0300

    Fix _store structure project Vuex

---
+++ src/resources/register/_store/index.ts
@@ -0,0 +1,27 @@
+import actions from './actions';
+import getters from './actions';
+import state from './actions';
+
+// const state = {
+
+// }
+
+// const actions = {
+  
+// }
+
+// const getters = {
+
+// }
+
+const mutation = {
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutation,
+  getters
+}
